b203b5b338204d52ff6ed88f287da0d93e9a49e5
我爱蓝莓 <56059199@qq.com>
2024-01-22 14:19:32 +0800

    虚拟支付打点日志

--- src/player/message-handlers/goodsApi.ts
+++ src/player/message-handlers/goodsApi.ts
@@ -109,7 +109,7 @@ export class GoodsApi extends BaseApi {
       offer_id: await this.service.utils.getGlobalConfigByName("offerid"),
       ts: Math.floor(Date.now() / 1000),
       zone_id: await this.service.utils.getGlobalConfigByName("zoneid"),
-      env: String(message.env),
+      env: message.env,
       user_ip: this.player.getIpAddress()
     }
 
