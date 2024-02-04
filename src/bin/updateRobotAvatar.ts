f0d759546db542015f22cad1f1269c0ecf2dc0b9
我爱蓝莓 <56059199@qq.com>
2024-02-04 14:56:05 +0800

    更新机器人头像

---
+++ src/bin/updateRobotAvatar.ts
@@ -0,0 +1,24 @@
+import Player from "../database/models/player";
+async function main() {
+  const players = await Player.find({robot: true});
+  const url = "https://im-serve.oss-cn-beijing.aliyuncs.com/uploads/images/";
+  const datas = [];
+
+  for (let i = 0; i < players.length; i++) {
+    if (i < 215) {
+      const avatar = `${url}${i + 1}.png`;
+      players[i].avatar = avatar;
+      datas.push(avatar);
+    } else {
+      const index = Math.floor(Math.random() * (215 - 1 + 1)) + 1;
+      const avatar = `${url}${index}.png`;
+      players[i].avatar = avatar;
+      datas.push(avatar);
+    }
+  }
+
+  console.log(datas);
+}
+
+main()
+
