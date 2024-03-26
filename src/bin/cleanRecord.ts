cb29bcd512de90696320e142bd95fd59fba4ab75
我爱蓝莓 <56059199@qq.com>
2024-03-26 15:37:45 +0800

    更新定时任务

--- src/bin/cleanRecord.ts
+++ src/bin/cleanRecord.ts
@@ -2,11 +2,13 @@ import * as moment from 'moment'
 import GameRecord from '../database/models/gameRecord'
 import GmNoteModel from "../database/models/gmNote";
 import RoomRecord from '../database/models/roomRecord'
+import GoldRecord from "../database/models/goldRecord";
 
 async function cleanRecord() {
   const sevenDayBefore = moment().subtract(7, 'days').startOf('day').toDate()
   const fiftyDayBefore = moment().subtract(50, 'days').startOf('day').toDate()
   await GameRecord.remove({time: {$lt: sevenDayBefore}}).exec()
+  await GoldRecord.remove({time: {$lt: sevenDayBefore}}).exec()
   await RoomRecord.remove({createAt: {$lt: fiftyDayBefore}}).exec()
   await GmNoteModel.remove({createAt: {$lt: sevenDayBefore}}).exec()
 }
--- src/schedules.js
+++ src/schedules.js
@@ -2,7 +2,6 @@ import * as schedule from 'node-schedule'
 import * as mongoose from 'mongoose'
 import * as config from './config'
 import * as moment from 'moment'
-import updateRank from './bin/updateRankList'
 import cleanRecord from './bin/cleanRecord'
 import fillPoorModel from './bin/fillUpRuby'
 import aggregateExtRecord from './bin/aggregateExtRecord'
@@ -13,17 +12,8 @@ import cleanUselessClub from "./bin/cleanUselessClub";
 import {service} from "./service/importService";
 import updateMnpAccessToken from "./bin/updateMnpAccessToken";
 import {updateTurntableTimes} from "./bin/updateMahjongSchedules";
-import updateRobotAvatar from "./bin/updateRobotAvatar";
 
 mongoose.connect(config.database.url)
-
-schedule.scheduleJob('0 0 * * 1', function () {
-  console.log(`update Rank`, new Date());
-  updateRank()
-    .catch(error => {
-      console.log('update Rank', error.stack)
-    })
-})
 
 schedule.scheduleJob('0 4 * * *', function () {
   console.log(`cleanRecord `, new Date());
@@ -31,33 +21,6 @@ schedule.scheduleJob('0 4 * * *', function () {
     .catch(error => {
       console.log('cleanRecord ', error.stack)
     })
-})
-
-schedule.scheduleJob('0 6 * * *', function () {
-  console.log(`give out poor model ruby`, new Date());
-  fillPoorModel()
-    .catch(error => {
-      console.log('give out poor model ruby', error.stack)
-    })
-})
-
-schedule.scheduleJob('59 * * * *', function () {
-  console.log(`aggregateExtRecord`, new Date());
-  aggregateExtRecord(new Date())
-    .catch(error => {
-      console.log('aggregateExtRecord error', error.stack)
-    })
-
-
-  var curDate = new Date();
-  if (curDate.getHours() < 2) {
-    curDate = moment().subtract(1, "day").toDate();
-    aggregateExtRecord(curDate)
-      .catch(error => {
-        console.log('aggregateExtRecord error', error.stack)
-      })
-  }
-
 })
 
 schedule.scheduleJob('0 */3 * * *', function () {
@@ -68,49 +31,12 @@ schedule.scheduleJob('0 */3 * * *', function () {
     })
 })
 
-
 schedule.scheduleJob('10 0 * * *', function () {
   console.log(`Room Record Log`, new Date());
   const from = moment().subtract(1, 'day').startOf('day').toDate()
   updateRoomCount(from.toISOString())
     .catch(error => {
       console.log('Room Record Log', error.stack)
-    })
-})
-
-schedule.scheduleJob('10 0 * * *', function () {
-  console.log(`club Room Record Log`, new Date());
-  const from = moment().subtract(1, 'day').startOf('day').toDate()
-  saveClubRoomInfo(from.toISOString())
-    .catch(error => {
-      console.log('club Room Record Log', error.stack)
-    })
-})
-
-// 定时删除俱乐部, 每天晚上 2 点执行
-schedule.scheduleJob('0 2 * * *', function () {
-  console.log(`clean club`, new Date());
-  cleanUselessClub()
-    .catch(error => {
-    console.log('cleanRecord ', error.stack)
-  })
-})
-
-// // 每天晚上 0 点检查开启排行榜
-// schedule.scheduleJob('1 0 * * *', function () {
-//   console.log(`open rank`, new Date());
-//   service.invite.initRank()
-//     .catch(error => {
-//       console.log('open rank error ', error.stack)
-//     })
-// })
-
-// 中午 12 点开始结算
-schedule.scheduleJob('0 12 * * *', function () {
-  console.log('settle rank 结算排行榜', new Date());
-  service.invite.settleRankPrize()
-    .catch(error => {
-      console.error('open rank error', error.stack)
     })
 })
 
@@ -130,10 +56,3 @@ schedule.scheduleJob('0 0 * * *', function () {
     })
 })
 
-// 每3分钟更新机器人头像
-// schedule.scheduleJob('*/3 * * * *', function () {
-//   updateRobotAvatar()
-//     .catch(error => {
-//       console.error('update robot avatar error', error.stack)
-//     })
-// })
