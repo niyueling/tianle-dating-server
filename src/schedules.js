236cc017250ac051dc9e6f3501f9a3b557e12d80
我爱蓝莓 <56059199@qq.com>
2024-04-08 12:36:31 +0800

    增加定时任务

--- src/schedules.js
+++ src/schedules.js
@@ -52,7 +52,7 @@ schedule.scheduleJob('0 0 * * *', function () {
 })
 
 // 每5分钟更新机器人对局状态
-schedule.scheduleJob('*/3 * * * *', function () {
+schedule.scheduleJob('* * * * *', function () {
   updateRobotGameState()
     .catch(error => {
       console.error('update player robot game state', error.stack)
