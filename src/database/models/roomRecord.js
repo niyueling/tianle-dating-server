48c32a0da1a9d65ac50b8fd243cfc7da4870cb96
我爱蓝莓 <56059199@qq.com>
2024-05-27 11:21:43 +0800

    增加战绩分享

--- src/database/models/roomRecord.js
+++ src/database/models/roomRecord.js
@@ -1,3 +1,28 @@
 'use strict';
-const { RoomRecord } = require('@fm/model/models/roomRecord');
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RoomRecordSchema = new Schema({
+    room: {type: String, required: true},
+    players: {type: [String], required: true, ref: 'Player'},
+    scores: {type: [], required: true},
+    createAt: {type: Date, required: true, default: Date.now},
+    roomNum: {type: Number, required: true},
+    creatorId: {type: Number },
+    rule: {type: Object, required: true},
+    category: {type: String, required: true, default: 'paodekuai'},
+    roomState: {type: String, required: true, default: 'normal'},
+    // 该房间玩到第几局
+    juIndex: {type: Number, required: false,},
+    // 本场大赢家
+    bigWinner: {type: [], required: false},
+});
+
+RoomRecordSchema.index({players: 1});
+RoomRecordSchema.index({room: 1});
+RoomRecordSchema.index({createAt: -1});
+RoomRecordSchema.index({club: 1, createAt: -1});
+
+const RoomRecord = mongoose.model('RoomRecord', RoomRecordSchema);
 export default RoomRecord;
