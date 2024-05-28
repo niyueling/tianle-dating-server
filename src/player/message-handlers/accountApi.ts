8f650b703ecdec74b48d20415c43f51a0fffd956
我爱蓝莓 <56059199@qq.com>
2024-05-28 14:18:56 +0800

    修改登录重连

--- src/player/message-handlers/accountApi.ts
+++ src/player/message-handlers/accountApi.ts
@@ -44,7 +44,7 @@ export class AccountApi extends BaseApi {
       user.disconnectedRoom = true;
     }
 
-    const allGameTypes = [GameType.mj, GameType.xueliu, GameType.guobiao];
+    const allGameTypes = [GameType.mj, GameType.xueliu, GameType.guobiao, GameType.pcmj, GameType.xmmj];
     for (let i = 0; i < allGameTypes.length; i++) {
       // 下发掉线子游戏
       const room = await service.roomRegister.getDisconnectRoomByPlayerId(user._id.toString(), allGameTypes[i]);
@@ -231,7 +231,7 @@ export class AccountApi extends BaseApi {
       model.disconnectedRoom = true;
     }
 
-    const allGameTypes = [GameType.mj, GameType.xueliu, GameType.guobiao, GameType.pcmj];
+    const allGameTypes = [GameType.mj, GameType.xueliu, GameType.guobiao, GameType.pcmj, GameType.xmmj];
     for (let i = 0; i < allGameTypes.length; i++) {
       // 下发掉线子游戏
       const room = await service.roomRegister.getDisconnectRoomByPlayerId(model._id.toString(), allGameTypes[i]);
