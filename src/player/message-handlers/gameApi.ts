3a00b05179b864655ed7bae4636f58d37a221443
我爱蓝莓 <56059199@qq.com>
2024-06-26 15:44:03 +0800

    求签返回道具数量

--- src/player/message-handlers/gameApi.ts
+++ src/player/message-handlers/gameApi.ts
@@ -120,7 +120,7 @@ export class GameApi extends BaseApi {
   @addApi()
   async blessByGem(player, message) {
     const list = await LuckyBless.find().sort({orderIndex: 1});
-    console.error(`_id ${message._id}`);
+    console.error(`message ${JSON.stringify(message)}`);
     let blessIndex = list.findIndex(bless => bless._id.toString() === message._id);
     let bless = list[blessIndex];
     if (!bless) {
