4b1abfc21f8816dde23b2084013a6d26eba165e1
我爱蓝莓 <56059199@qq.com>
2024-07-02 17:01:46 +0800

    修复7日登录无法领取bug

--- src/player/message-handlers/loginSignApi.ts
+++ src/player/message-handlers/loginSignApi.ts
@@ -42,13 +42,14 @@ export class LoginSignApi extends BaseApi {
     }
 
     // 判断是否领取
-    const start = moment(new Date()).startOf('day').toDate();
-    const end = moment(new Date()).endOf('day').toDate();
-    const receive = await SevenSignPrizeRecord.findOne({playerId: this.player._id, "prizeConfig.day": prizeInfo.day, createAt: {$gte: start, $lt: end}});
-
-    // if (receive) {
-    //   return this.replyFail(TianleErrorCode.prizeIsReceive);
-    // }
+    const receive = await SevenSignPrizeRecord.findOne({playerId: this.player._id, "prizeConfig.day": prizeInfo.day});
+    if (receive) {
+      if (Date.parse(receive.createAt) > new Date().setHours(0, 0, 0 , 0)) {
+        return this.replyFail(TianleErrorCode.prizeIsReceive);
+      } else {
+        await SevenSignPrizeRecord.remove({playerId: this.player._id, "prizeConfig.day": prizeInfo.day});
+      }
+    }
 
     // 按照奖励类型领取奖励
     await this.receivePrize(prizeInfo, this.player._id, message.multiple);
