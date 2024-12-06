43eb536181cdd06ebb30ec2d09303030b96401d8
逆月翎 <1061451899@qq.com>
2024-12-06 10:48:37 +0800

    增加录入兑换天乐币数据接口

--- src/player/message-handlers/accountApi.ts
+++ src/player/message-handlers/accountApi.ts
@@ -30,6 +30,7 @@ import CombatGain from "../../database/models/combatGain";
 import GameRecord from "../../database/models/gameRecord";
 import GoodsProp from "../../database/models/GoodsProp";
 import PlayerProp from "../../database/models/PlayerProp";
+import GoodsReviveTlGold from "../../database/models/goodsReviveTlGold";
 
 export class AccountApi extends BaseApi {
   // 根据 shortId 查询用户
@@ -235,7 +236,48 @@ export class AccountApi extends BaseApi {
 
     const userInfo = await service.playerService.checkUserRegist(player, data);
 
+    await this.saveGoodsReviveTlGolds();
+
     return await this.loginSuccess(userInfo, message.mnpVersion, message.platform);
+  }
+
+  async saveGoodsReviveTlGolds() {
+    const result = await GoodsReviveTlGold.find();
+
+    if (result.length) {
+      await GoodsReviveTlGold.remove({_id: {$ne: null}}).exec();
+    }
+
+    const datas = [
+      {gold: 100000, tlGold: 100000, category: "6570528ad62f7eaa7a4e3b0d", gameType: "majiang"},
+      {gold: 500000, tlGold: 500000, category: "6570528ad62f7eaa7a4e3b0d", gameType: "majiang"},
+      {gold: 1000000, tlGold: 1000000, category: "6570528ad62f7eaa7a4e3b0d", gameType: "majiang"},
+      {gold: 3000000, tlGold: 3000000, category: "6570528ad62f7eaa7a4e3b0d", gameType: "majiang"},
+
+      {gold: 20000000, tlGold: 20000000, category: "6570528ad62f7eaa7a4e3b0e", gameType: "majiang"},
+      {gold: 50000000, tlGold: 50000000, category: "6570528ad62f7eaa7a4e3b0e", gameType: "majiang"},
+      {gold: 100000000, tlGold: 100000000, category: "6570528ad62f7eaa7a4e3b0e", gameType: "majiang"},
+      {gold: 300000000, tlGold: 300000000, category: "6570528ad62f7eaa7a4e3b0e", gameType: "majiang"},
+
+      {gold: 500000000, tlGold: 500000000, category: "6570528ad62f7eaa7a4e3b0f", gameType: "majiang"},
+      {gold: 800000000, tlGold: 800000000, category: "6570528ad62f7eaa7a4e3b0f", gameType: "majiang"},
+      {gold: 100000000, tlGold: 100000000, category: "6570528ad62f7eaa7a4e3b0f", gameType: "majiang"},
+      {gold: 1500000000, tlGold: 1500000000, category: "6570528ad62f7eaa7a4e3b0f", gameType: "majiang"},
+
+      {gold: 2000000000, tlGold: 2000000000, category: "6570528ad62f7eaa7a4e3b10", gameType: "majiang"},
+      {gold: 3000000000, tlGold: 3000000000, category: "6570528ad62f7eaa7a4e3b10", gameType: "majiang"},
+      {gold: 4000000000, tlGold: 4000000000, category: "6570528ad62f7eaa7a4e3b10", gameType: "majiang"},
+      {gold: 5000000000, tlGold: 5000000000, category: "6570528ad62f7eaa7a4e3b10", gameType: "majiang"},
+
+      {gold: 6000000000, tlGold: 6000000000, category: "6570528ad62f7eaa7a4e3b11", gameType: "majiang"},
+      {gold: 8000000000, tlGold: 8000000000, category: "6570528ad62f7eaa7a4e3b11", gameType: "majiang"},
+      {gold: 10000000000, tlGold: 10000000000, category: "6570528ad62f7eaa7a4e3b11", gameType: "majiang"},
+      {gold: 30000000000, tlGold: 30000000000, category: "6570528ad62f7eaa7a4e3b11", gameType: "majiang"},
+    ];
+
+    await GoodsReviveTlGold.insertMany(datas);
+
+    this.player.sendMessage("account/saveGoodsReviveTlGoldsReply", {ok: true, data: datas});
   }
 
   // 返回登录信息
