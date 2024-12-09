25857d9828770fc9446725744a0450cc44e7b41d
逆月翎 <1061451899@qq.com>
2024-12-09 20:00:56 +0800

    增加兑换天乐币接口

--- src/player/message-handlers/goodsApi.ts
+++ src/player/message-handlers/goodsApi.ts
@@ -1383,7 +1383,7 @@ export class GoodsApi extends BaseApi {
   // 兑换天乐币列表
   @addApi()
   async getReviveTlGoldList(message) {
-    const reviveList = await GoodsReviveTlGold.find({ category: message.category, gameType: message.gameType }).sort({gold: 1});
+    const reviveList = await GoodsReviveTlGold.find({ gameType: message.gameType }).sort({gold: 1});
 
     this.replySuccess(reviveList);
   }
