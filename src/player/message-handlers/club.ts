3799d6fb167c5128f407ac8337edf3654b73ec01
逆月翎 <1061451899@qq.com>
2024-12-31 10:36:20 +0800

    修复合伙人统计数据不显示

--- src/player/message-handlers/club.ts
+++ src/player/message-handlers/club.ts
@@ -830,8 +830,6 @@ export default {
         minDate.setMilliseconds(0);
         minDate.setDate(minDate.getDate() - await getPartnerDate(message.type));
 
-        console.warn("day-%s, minDate-%s", await getPartnerDate(message.type), minDate);
-
         const params = {club: myClub._id, partner: true};
         if (message.playerShortId) {
             const searchInfo = await PlayerModel.findOne({shortId: message.playerShortId});
@@ -1548,10 +1546,12 @@ async function getPartnerDate(type) {
 }
 
 async function getRoomCountByGame(club, member, minDate) {
+
     const params = {
         club: club._id, scores: {$ne: []},
         players: {$ne: []}, createAt: {$gt: minDate}
     }
+    console.warn(params);
     const records = await RoomRecord
         .find(params)
         .sort({createAt: -1})
