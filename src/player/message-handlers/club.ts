e6b5f1a58f6f6151f5fa30030d84f192ef5fa95c
逆月翎 <1061451899@qq.com>
2025-01-13 19:02:42 +0800

    用户加入战队通知列表更新

--- src/player/message-handlers/club.ts
+++ src/player/message-handlers/club.ts
@@ -620,7 +620,7 @@ export default {
       messageLists = [...messageLists, ...clubRequestInfo, ...clubMergeInfo];
     }
 
-    messageLists.sort((a, b) => b.createdAt - a.createdAt);
+    messageLists = messageLists.sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
 
     return player.replySuccess(ClubAction.getRequestInfo, {requestList: messageLists});
   },
