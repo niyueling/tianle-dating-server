47c8f43cfb05c9062c8d1242fc8718c2dfff34dc
逆月翎 <1061451899@qq.com>
2025-02-17 14:51:44 +0800

    定时清除数据

--- src/bin/cleanRecord.ts
+++ src/bin/cleanRecord.ts
@@ -6,14 +6,16 @@ import GmNoteModel from "../database/models/gmNote";
 import GoldRecord from "../database/models/goldRecord";
 import RoomRecord from '../database/models/roomRecord'
 import RoomScoreRecord from "../database/models/roomScoreRecord";
+import DiamondRecord from "../database/models/diamondRecord";
 
 async function cleanRecord() {
   const sevenDayBefore = moment().subtract(7, 'days').startOf('day').toDate()
   const fiftyDayBefore = moment().subtract(50, 'days').startOf('day').toDate()
   await GameRecord.remove({time: {$lt: sevenDayBefore}}).exec()
-  await GoldRecord.remove({time: {$lt: sevenDayBefore}}).exec()
-  await RoomScoreRecord.remove({time: {$lt: sevenDayBefore}}).exec()
-  await GameCardRecord.remove({time: {$lt: sevenDayBefore}}).exec()
+  await RoomScoreRecord.remove({createAt: {$lt: sevenDayBefore}}).exec()
+  await GameCardRecord.remove({createAt: {$lt: sevenDayBefore}}).exec()
+  await GoldRecord.remove({createAt: {$lt: sevenDayBefore}}).exec()
+  await DiamondRecord.remove({createAt: {$lt: sevenDayBefore}}).exec()
   await CombatGain.remove({time: {$lt: sevenDayBefore}}).exec()
   await RoomRecord.remove({createAt: {$lt: fiftyDayBefore}}).exec()
   await GmNoteModel.remove({createAt: {$lt: sevenDayBefore}}).exec()
